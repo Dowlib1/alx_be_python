a49242516f28d5969243d038ed1170461b0fbacd
dowlib1 <oladimeji0107@.com>
2025-09-14 16:15:26 +0100

    redefined Principal interest calculation, based on alx's requirement

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,6 +1,6 @@
 principal = 1000
-rate = 0.05
+rate = 5
 time = 3
 
-simple_interest = (principal * rate * time) 
+simple_interest = (principal * rate * time) / 100 # Calculate simple interest
 print("The simple interest is:", simple_interest)
