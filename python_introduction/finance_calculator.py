fa838f306fa55ce6889ee53e85d76c09770918bf
dowlib1 <oladimeji0107@.com>
2025-09-14 20:34:57 +0100

    adjusted variable names

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,8 +1,8 @@
-monthly_income = float(input("Enter your monthly income: "))
-monthly_expenses = float(input("Enter your total monthly expenses: "))
+monthly_income = input("Enter your monthly income: ")
+monthly_expenses = input("Enter your total monthly expenses: ")
 
 monthly_savings = float(monthly_income) - float(monthly_expenses)
 print("Your monthly savings is: ", monthly_savings)
 
-projected_annual_savings = float(monthly_savings * 12 + float(monthly_savings * 0.05 * 12))
-print(f"Projected savings after one year, with interest, is: {projected_annual_savings}")
+annual_savings = float(monthly_savings * 12 + float(monthly_savings * 0.05 * 12))
+print(f"Projected savings after one year, with interest, is: annual_savings")
