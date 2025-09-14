fe6299ef7bfe5012d00c9fd120203e90bc089144
dowlib1 <oladimeji0107@.com>
2025-09-14 18:12:49 +0100

    defined age calculator logic

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,4 +1,4 @@
-age = input('what is your age? ')
+age = input('How old are you? ')
 future_age = int(age) + 27
 current_year = 2050
-print('In the year', current_year, 'you will be', future_age, 'years old.')
+print('In the', current_year, 'you will be', future_age, 'years old.')
