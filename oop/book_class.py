2e68ddbbef676428cb66989a262677cc5b5028f5
dowlib1 <oladimeji0107@gmail.com>
2025-10-12 17:44:27 -0700

    initial commit for boo_class.py, classic_static_methods.py, library_system.py, polymrphism_demo.py

---
+++ oop/book_class.py
@@ -0,0 +1,14 @@
+class Book:
+    def _init__(self, title, author, year):
+        self.tittle = title
+        self.author = author
+        self.year = year
+
+    def __del__(self):
+        print(f"Deleting {self.title}")
+
+    def __str__(self):
+        return f"{self.title} by {self.author), published in {self.year}"
+
+    def __repr__(self):
+        return f"Book('{self.title}', '{self.author}', {self.year})"
